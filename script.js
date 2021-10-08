2537fe0a6af3332b382152bdf708b04ef78f01c5
sameem786-blip <sameemabbas2002@outlook.com>
2021-10-08 13:36:00 +0500

    abc

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-// const sum = () => {
-//     let v1 = Number(document.getElementById('i1').value);
-//     let v2 = Number(document.getElementById('i2').value);
-
-//     let sum = v1 + v2;
-
-//     document.getElementById(sbtn).value = sum;
-// }
-
-// document.getElementById('sbtn').addEventListener('click',sum());
 const sum = () => {
     let v1 = Number(document.getElementById('i1').value);
     let v2 = Number(document.getElementById('i2').value);
